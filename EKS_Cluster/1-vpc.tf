1319bd46b8bf611611f94c9a300ff6db28dfc91f
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-10-27 19:01:27 +0530

    Create 1-vpc.tf

---
+++ EKS_Cluster/1-vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "EKS_VPC"
+  }
+}
