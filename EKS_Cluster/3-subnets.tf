a9fead82eed9f5cd45170b60a41dec975f44819c
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-10-27 19:10:42 +0530

    Create 3-subnets.tf

---
+++ EKS_Cluster/3-subnets.tf
@@ -0,0 +1,49 @@
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name"                            = "eks-private-us-east-1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "shared"
+  }
+}
+
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name"                            = "eks-private-us-east-1b"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "shared"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "eks-public-us-east-1a"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "shared"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "eks-public-us-east-1b"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "shared"
+  }
+}
