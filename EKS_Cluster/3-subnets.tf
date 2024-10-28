fae78384d22186ff3164ef1d24b753515b347637
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-10-28 21:53:09 +0530

    Update 3-subnets.tf

--- EKS_Cluster/3-subnets.tf
+++ EKS_Cluster/3-subnets.tf
@@ -1,12 +1,12 @@
 resource "aws_subnet" "private-us-east-1a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
     "Name"                            = "eks-private-us-east-1a"
-    "kubernetes.io/role/internal-elb" = "1"          # discover the subnets over private LB
-    "kubernetes.io/cluster/demo"      = "shared"     # if you want to share with other k8 clusters
+    "kubernetes.io/role/internal-elb" = "1"      # discover the subnets over private LB
+    "kubernetes.io/cluster/demo"      = "shared" # if you want to share with other k8 clusters
   }
 }
 
