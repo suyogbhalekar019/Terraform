e1ee411d574666820fbf55d13f12f21a43e14b91
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-10-28 21:51:17 +0530

    Update 1-vpc.tf

--- EKS_Cluster/1-vpc.tf
+++ EKS_Cluster/1-vpc.tf
@@ -1,12 +1,18 @@
+locals {
+  cluster_name = "suyog-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+}
+
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "EKS_VPC"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
   }
 }
