4f8bf40035e15dbf7e18419df3bd9bce3c7ec536
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-10-29 15:05:20 +0530

    Update 4-nat.tf

--- EKS_Cluster/4-nat.tf
+++ EKS_Cluster/4-nat.tf
@@ -1,13 +1,21 @@
-resource "aws_eip" "nat" {
+resource "aws_eip" "nat-1" {
   domain = "vpc"
 
   tags = {
-    Name = "eks-nat"
+    Name = "eks-nat-1"
+  }
+}
+
+resource "aws_eip" "nat-2" {
+  domain = "vpc"
+
+  tags = {
+    Name = "eks-nat-2"
   }
 }
 
 resource "aws_nat_gateway" "nat1" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat-1.id
   subnet_id     = aws_subnet.public-us-east-1a.id
 
   tags = {
@@ -18,11 +26,11 @@ resource "aws_nat_gateway" "nat1" {
 }
 
 resource "aws_nat_gateway" "nat2" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat-2.id
   subnet_id     = aws_subnet.public-us-east-1b.id
 
   tags = {
-    Name = "eks-nat2"
+    Name = "eks-nat-2"
   }
 
   depends_on = [aws_internet_gateway.igw]
