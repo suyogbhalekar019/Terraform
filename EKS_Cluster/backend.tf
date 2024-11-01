e86596b1a50019d81ef3f5f4853c76bd09595da3
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-11-01 11:55:38 +0530

    Update backend.tf

--- EKS_Cluster/backend.tf
+++ EKS_Cluster/backend.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   backend "s3" {
-    bucket         = "my-backend-bucket"                      # Replace with your bucket name
+    bucket         = "suyog-unique-my-backend-bucket"         # Replace with your bucket name
     key            = "global/s3/terraform.tfstate"            # Path within the bucket
-    region         = "us-east-1"                              # Replace with your AWS region
+    region         = "us-east-1"                              # Replace w"suyog-unique-my-backend-bucket"ith your AWS region
     dynamodb_table = "terraform-locks"                        # State locking with DynamoDB
     encrypt        = true                                     # Enable server-side encryption
   }
