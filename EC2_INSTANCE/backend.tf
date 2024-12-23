02eac6e116e016932a61bffe6ef51f3a5eb486c0
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-12-23 22:28:57 +0530

    Create backend.tf

---
+++ EC2_INSTANCE/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "custom-unique-my-backend-bucket"         # Replace with your bucket name
+    key            = "s3/terraform.tfstate"            # Path within the bucket
+    region         = "us-east-1"                              # Replace w"suyog-unique-my-backend-bucket"ith your AWS region
+    dynamodb_table = "terraform-locks"                        # State locking with DynamoDB
+    encrypt        = true                                     # Enable server-side encryption
+  }
+}
