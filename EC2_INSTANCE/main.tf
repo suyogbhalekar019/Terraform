c38ee35776d5275ef9da020f6565545fce093af2
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-12-23 18:02:32 +0530

    Update main.tf

--- EC2_INSTANCE/main.tf
+++ EC2_INSTANCE/main.tf
@@ -6,7 +6,18 @@ resource "aws_instance" "MyInstance" {
     ami           = "ami-0c55b159cbfafe1f"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"              # Change to your desired instance type
 
+    count = 3
+
     tags = {
-      Name = "InstanceName"                 # This sets the Name Tag
+      Name = "InstanceName-${count.index}"                 # This sets the Name Tag
+    }
+
+    vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    subnet_ids = [
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+      ]
     }
 }
