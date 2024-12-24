020361ddb7d66937d9869117f4d243eeddd71919
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-12-24 13:32:02 +0530

    Create main.tf

---
+++ EC2-INSTANCE/S3/main.tf
@@ -0,0 +1,39 @@
+# main.tf
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.s3_bucket_name
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  hash_key = "LockID"
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}
