3e14f477eb48ddbfe06f107c76ebf5452581468c
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-12-24 13:17:54 +0530

    Create subnet.tf

---
+++ EC2-INSTANCE/subnet.tf
@@ -0,0 +1,12 @@
+# subnet.tf
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
