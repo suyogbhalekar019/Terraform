3301c1a923cf705d5ce93ab3ad948dd8bb003a7b
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-12-30 16:06:32 +0530

    Update 7-backend.tf

--- EC2-INSTANCE/7-backend.tf
+++ EC2-INSTANCE/7-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket" # Replace with your S3 bucket name
+    bucket         = "EC2-S3_bucket-state-lock" # Replace with your S3 bucket name
     key            = "terraform/state.tfstate"    # Path inside the bucket
     region         = "us-east-1"                  # S3 bucket region
     dynamodb_table = "terraform-lock-table"       # DynamoDB table for state locking
